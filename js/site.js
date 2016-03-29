6ed871f6a9678d9621c3563c08493969864bde8f
801cucumber <psyche.delicha@gmail.com>
2016-03-30 12:15:47 +1300

    Fix hotspot

--- js/site.js
+++ js/site.js
@@ -1,6 +1,7 @@
 $('.level-toggle').click(function() {
   $('.level-content').slideUp(200);
   $(this).closest('.level').find('.level-content').slideToggle(200, function() {
+    $('map').imageMapResize();
   });
 });
 
