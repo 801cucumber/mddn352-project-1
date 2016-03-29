35d12202e1e422f554ffd9544fada21eef0a5ac0
801cucumber <psyche.delicha@gmail.com>
2016-03-29 13:11:48 +1300

    Edit footer

--- js/site.js
+++ js/site.js
@@ -5,13 +5,12 @@ $('.level-toggle').click(function() {
 
 // a cross reference of area names to text to be shown for each area
  var xref = {
-     cafe: "<b>Carrots</b> are delicious and may turn your skin orange!",
-     store: "<b>Asparagus</b> is one of the first vegetables of the spring. "
-         +"Being a dark green, it's great for you, and has interesting side effects."
+     cafe: "<b>Te Papa Cafe</b> has stuff you eat!",
+     store: "<b>Te Papa Store</b> has stuff you buy. "
+         +"Give us the money."
  };
 
- var defaultDipTooltip = 'I know you want the dip. But it\'s loaded with saturated fat, just skip it '
-     +'and enjoy as many delicious, crisp vegetables as you can eat.';
+ var defaultDipTooltip = '';
 
  var image = $('#level-1-map');
 
@@ -20,9 +19,9 @@ $('.level-toggle').click(function() {
      fillOpacity: 0.4,
      fillColor: "d42e16",
      stroke: true,
-     strokeColor: "3320FF",
+     strokeColor: "038DB1",
      strokeOpacity: 0.8,
-     strokeWidth: 4,
+     strokeWidth: 2,
      singleSelect: true,
      mapKey: 'name',
      listKey: 'name',
@@ -32,12 +31,6 @@ $('.level-toggle').click(function() {
          // update text depending on area selected
          $('#selections').html(xref[e.key]);
 
-         // if Asparagus selected, change the tooltip
-         if (e.key === 'cafe') {
-             newToolTip = "OK. I know I have come down on the dip before, but let's be real. "
-                 +"Raw asparagus without any of that delicious ranch and onion dressing "
-                 +"slathered all over it is not so good.";
-         }
          image.mapster('set_options', {
              areas: [{
                  key: "dip",
@@ -54,7 +47,7 @@ $('.level-toggle').click(function() {
          },
          {
              key: "store",
-             fillColor: "000000"
+             fillColor: "ffffff"
          }
          ]
  });
